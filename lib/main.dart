e208a3e61efa04de102e2ff6141fa3bc729daac8
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-04-21 22:31:20 -0300

    Add Firebase suport

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:tic_tac_toe/firebase_options.dart';
 import 'package:tic_tac_toe/presenter/pages/main_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   setUrlStrategy(PathUrlStrategy());
 
   runApp(const MainApp());
