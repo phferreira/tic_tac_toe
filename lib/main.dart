dd12ea4cd29ee14447c9c45f1743b485ba7ec1c7
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2025-01-22 13:25:38 -0300

    feat(Local Storage): Add shared preferences to store local data

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tic_tac_toe/firebase_options.dart';
 import 'package:tic_tac_toe/presenter/pages/main_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+late final SharedPreferences sharedPreferences;
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   setUrlStrategy(PathUrlStrategy());
+
+  sharedPreferences = await SharedPreferences.getInstance();
 
   runApp(const MainApp());
 }
